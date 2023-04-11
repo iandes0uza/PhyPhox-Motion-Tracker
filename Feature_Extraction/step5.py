7add9c4d0fef13fdc4451f8b50ea88d2472b8235
iandes0uza <ianagdesouza2002@gmail.com>
2023-04-10 23:47:13 -0400

    updated sets

--- Feature_Extraction/step5.py
+++ Feature_Extraction/step5.py
@@ -4,58 +4,35 @@ import pandas as pd
 from sklearn import preprocessing
 
 #INPUT DATA USING HDF5 (maybe use different sets?)
-walking_train = pd.read_csv('input_data\walking_train.csv')
-jumping_train = pd.read_csv('input_data\jumping_train.csv')
+trained_data = pd.read_csv('input_data\trained.csv')
 
 #HAVE A PREDETERMINED WINDOW SIZE
 window_size = 31
 
 #PERFORM FEATURE EXTRACTION ON WALK
-mean_walk = pd.Series(walking_train).rolling(window_size).mean()
-std_walk = pd.Series(walking_train).rolling(window_size).std()
-max_walk = pd.Series(walking_train).rolling(window_size).max()
-min_walk = pd.Series(walking_train).rolling(window_size).min()
-median_walk = pd.Series(walking_train).rolling(window_size).median()
-var_walk = pd.Series(walking_train).rolling(window_size).var()
-corr_walk = pd.Series(walking_train).rolling(window_size).corr()
-cov_walk = pd.Series(walking_train).rolling(window_size).cov()
-skew_walk = pd.Series(walking_train).rolling(window_size).skew()
-
-#PERFORM FEATURE EXTRACTION ON JUMP
-mean_jump = pd.Series(jumping_train).rolling(window_size).mean()
-std_jump = pd.Series(jumping_train).rolling(window_size).std()
-max_jump = pd.Series(jumping_train).rolling(window_size).max()
-min_jump = pd.Series(jumping_train).rolling(window_size).min()
-median_jump = pd.Series(jumping_train).rolling(window_size).median()
-var_jump = pd.Series(jumping_train).rolling(window_size).var()
-corr_jump = pd.Series(jumping_train).rolling(window_size).corr()
-cov_jump = pd.Series(jumping_train).rolling(window_size).cov()
-skew_jump = pd.Series(jumping_train).rolling(window_size).skew()
+mean_train = pd.Series(trained_data).rolling(window_size).mean()
+std_train = pd.Series(trained_data).rolling(window_size).std()
+max_train = pd.Series(trained_data).rolling(window_size).max()
+min_train = pd.Series(trained_data).rolling(window_size).min()
+median_train = pd.Series(trained_data).rolling(window_size).median()
+var_train = pd.Series(trained_data).rolling(window_size).var()
+corr_train = pd.Series(trained_data).rolling(window_size).corr()
+cov_train = pd.Series(trained_data).rolling(window_size).cov()
+skew_train = pd.Series(trained_data).rolling(window_size).skew()
 
 #EXTRACT FEATURES FOR WALK AND JUMP
-features_walking = pd.DataFrame({'mean': mean_walk,
-                                 'std': std_walk,
-                                 'max': max_walk,
-                                 'min': min_walk,
-                                 'median': median_walk,
-                                 'var': var_walk,
-                                 'corr': corr_walk,
-                                 'cov': cov_walk,
-                                 'skew': skew_walk})
-
-features_jumping = pd.DataFrame({'mean': mean_jump,
-                                 'std': std_jump,
-                                 'max': max_jump,
-                                 'min': min_jump,
-                                 'median': median_jump,
-                                 'var': var_jump,
-                                 'corr': corr_jump,
-                                 'cov': cov_jump,
-                                 'skew': skew_jump})
+features = pd.DataFrame({'mean': mean_train,
+                         'std': std_train,
+                         'max': max_train,
+                         'min': min_train,
+                         'median': median_train,
+                         'var': var_train,
+                         'corr': corr_train,
+                         'cov': cov_train,
+                         'skew': skew_train})
 
 #DROP NAN's PRODUCED BY WINDOW
-features_walking = features_walking.dropna()
-features_jumping = features_jumping.dropna()
+features = features.dropna()
 
 #EXPORT BACK TO HDF5
 
