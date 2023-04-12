2a483f45c8e9e63454c46c526957f43e2c87d05c
iandes0uza <ianagdesouza2002@gmail.com>
2023-04-12 18:01:16 -0400

    making features wrap around window sizes

--- Project_390/step5.py
+++ Project_390/step5.py
@@ -6,33 +6,40 @@ from sklearn import preprocessing
 
 
 # #INPUT DATA USING HDF5 (maybe use different sets?)
-# with h5py.File('input_data/data.h5', 'r') as hdf:
-#     trained_data = hdf['/dataset/training/Train_Data']
-#     print(trained_data)
-#     print("MEOW MEOW MEOW\n\n\n\n\n\n\n")
+processed_df = h5py.File('./output_data/data.h5', 'r+')['dataset/training/Processed_Data']
+with h5py.File('./output_data/data.h5', 'r+') as f:
+    # # Open the source and destination datasets
+
+    for i in range(len(processed_df)):
+        data = pd.DataFrame(processed_df[i])
+        feature_data = data.iloc[:, 0:-1]
+        features = pd.DataFrame(columns=['mean', 'std', 'min', 'max', 'skew'])
+        window_size = 31
+        features['mean'] = feature_data.rolling(window = window_size).mean()
+        # features['std'] = data.iloc[:, 1:-1].rolling(window = window_size).std()
+        # features['min'] = data.iloc[:, 1:-1].rolling(window = window_size).min()
+        # features['max'] = data.iloc[:, 1:-1].rolling(window = window_size).max()
+        # features['skew'] = data.iloc[:, 1:-1].rolling(window = window_size).skew()
+        print(features)
+
+
+    # train_df = f['dataset/training/Train_Data']
+    f.create_dataset('dataset/training/Processed_Data', data=features)
+
+# features = trained_data.agg(['min', 'max', 'mean', 'median', 'var', 'skew', 'std'])
+# features.columns = ['_'.join(col).strip() for col in features.columns.values]
+
+# print(features)
+
+# # DROP NAN VALUES
+# features = features.dropna()
+
+# # OUTPUT DATA
+# features.to_csv('output_data/train_features.csv', index=False)
+
+# # #EXPORT BACK TO HDF5
+# # with h5py.File('output_data/data.h5', 'a') as hdf:
     
-# INPUT DATA
-trained_data = pd.read_csv('input_data/train.csv')
-
-# print(trained_data)
-
-# PERFORM FEATURE EXTRACTION
-window_size = 31
-
-features = trained_data.agg(['min', 'max', 'mean', 'median', 'var', 'skew', 'std'])
-features.columns = ['_'.join(col).strip() for col in features.columns.values]
-
-print(features)
-
-# DROP NAN VALUES
-features = features.dropna()
-
-# OUTPUT DATA
-features.to_csv('output_data/train_features.csv', index=False)
-
-# #EXPORT BACK TO HDF5
-# with h5py.File('output_data/data.h5', 'a') as hdf:
-    
-#     group = hdf['/dataset/training']
-#     dataset = group.create_dataset('Features', data=features.to_numpy())
+# #     group = hdf['/dataset/training']
+# #     dataset = group.create_dataset('Features', data=features.to_numpy())
     
