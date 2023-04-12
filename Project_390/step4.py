7cf9b667dc31cbd455f71fcfb5eda3814abf7f7a
iandes0uza <ianagdesouza2002@gmail.com>
2023-04-12 17:00:35 -0400

    finalized preprocessing

--- Project_390/step4.py
+++ Project_390/step4.py
@@ -5,25 +5,36 @@ import h5py
 from sklearn import preprocessing
 
 # INPUT DATA SET
-with h5py.File('output_data/data.h5', 'r') as hdf:
-    train_df = hdf['dataset/training/Train_Data']
+# with h5py.File('output_data/data.h5', 'r') as hdf:
+train_df = h5py.File('./output_data/data.h5', 'r+')['dataset/training/Train_Data']
+with h5py.File('./output_data/data.h5', 'r+') as f:
+    # # Open the source and destination datasets
+    # train_df = f['dataset/training/Train_Data']
+    processed_df = f.create_dataset('dataset/training/Processed_Data', data=train_df)
 
-# NORMALIZE
-for sub_array in train_df:
-    for element in sub_array:
-        train_data = element.iloc[:, 1:3]
+    for i in range(len(train_df)):
+        # NORMALIZE
+        data = pd.DataFrame(train_df[i])
+        train_data = data.iloc[:, 0:-1]
+        train_label = data.iloc[:, 5]
         sc = preprocessing.StandardScaler()
         train = sc.fit_transform(train_data)
-        element = pd.DataFrame(train, columns=train_data.columns)
+        normalized_data = pd.DataFrame(train, columns=train_data.columns)
 
         # DECLARE WINDOW SIZE
         window_size = 31
-        element.rolling(window_size, center=True).mean()
+        normalized_data.rolling(window_size, center=True).mean()
+
+        # PUT THEM TOGETHER
+        result = pd.concat([normalized_data, train_label], axis=1)
+        print(result)
+        processed_df[i] = result
 
 
-train_df.plot()
-plt.show()
 
-# OUTPUT DATA SET
-# filtered_train.to_csv('output_data/train_filtered.csv', index=False)
+    # train_df.plot()
+    # plt.show()
+
+    # OUTPUT DATA SET
+    # filtered_train.to_csv('output_data/train_filtered.csv', index=False)
 
